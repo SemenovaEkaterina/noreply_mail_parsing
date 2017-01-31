b390dab19ac20a991a30647f15856c3c629ad9a5
kat-semyonova@mail.ru <kat-semyonova@mail.ru>
2017-01-31 17:59:06 +0300

    cor

--- parser.py
+++ parser.py
@@ -29,6 +29,7 @@ class HTML(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.text = ''
+        self.style = False
 
     def handle_starttag(self, tag, attrs):
         self.text += '<' + tag
@@ -39,12 +40,16 @@ class HTML(HTMLParser):
             self.text += '\n'
 
     def handle_endtag(self, tag):
-        self.text += '</' + tag + '>'
-        if tag != 'a' and tag != 'span':
-            self.text += '\n'
+        if tag != 'br':
+            self.text += '</' + tag + '>'
+            if tag != 'a' and tag != 'span':
+                self.text += '\n'
 
     def handle_data(self, data):
         self.text += data
+
+    def handle_comment(self, data):
+        pass
 
 
 
@@ -61,24 +66,34 @@ pre_patterns = [
     (re.compile(r'\n\t\n'), '\n')
 ]
 
-post_patterns = [
+
+datetime_patterns = [
     (re.compile(r'\n.+20\d\d ?г., \d{1,2}:\d\d пользователь.+<.+@.+\..+> ?[\n]?написал:.+\n'), '\n'),
     (re.compile(r'\n.+[П,п]ользователь.+Электронный журнал.+[\n]?написал.+\n'), '\n'),
     (re.compile(r'\n\d\d\.\d\d\.20\d\d \d?\d:\d\d, Электронный журнал.*[\n]?пишет:.*\n'), '\n'),
     (re.compile(r'\n.+Электронный журнал.+noreply@eljur.ru.*\n'), '\n'),
     (re.compile(r'\n.+ 20\d\d ?г?\.?,? \d?\d:\d\d .+ от Электронный журнал .+noreply@eljur.ru.+:.+\n'), '\n'),
-    #(re.compile(r'\n.+-------- Исходное сообщение --------.+[\n]+Дата: .+[\n]+Кому: .+[\n]+Тема: .+\n'), '\n'),
-    (re.compile('\n ?Отправлено с[о]? .+\n'), '\n'),
-    (re.compile(r'\n ?Отправлено из .+\n'), '\n'),
-    (re.compile(r'\n-- ?\n'), '\n'),
-    (re.compile(r'\n{2,}'), '\n')
+    (re.compile(r'\n-------- Исходное сообщение --------(\s*<.+>\s*)*[\n]+(\s*<.+>\s*)*Дата: ?.+(\s*<.+>\s*)*[\n]+(\s*<.+>\s*)*Кому: ?.+(\s*<.+>\s*)*[\n]+(\s*<.+>\s*)*Тема: ?.+(\s*<.+>\s*)*\n'), '\n'),
+    #(re.compile(r'\n(\s*<.+>\s*)*From: ?.+(\s*<.+>\s*)*[\n]+(\s*<.+>\s*)*Sent: ?.+(\s*<.+>\s*)*[\n]+(\s*<.+>\s*)*To: ?.+(\s*<.+>\s*)*[\n]+(\s*<.+>\s*)*Subject: ?.+(\s*<.+>\s*)*\n'), '\n'),
 ]
+
+post_patterns = [
+    (re.compile(r'\n<blockquote.+>(\s*<.+>\s*)*</blockquote>\n'), '\n'),
+    (re.compile(r'\n ?Отправлено с[о]? .+\n'), '\n'),
+    (re.compile(r'\n ?Отправлено из .+\n'), '\n'),
+    (re.compile(r'\n ?Sent from .+\n'), '\n'),
+    #(re.compile(r'\n(\s*<.+>\s*)*[^!]--[^>]-? ?(\s*<.+>\s*)*\n'), '\n'),
+    (re.compile(r'>?[\s]*---? ?'), '>\n'),
+    (re.compile(r'\n> ?\n'), '\n'),
+    (re.compile(r'\n{2,}'), '\n'),
+    (re.compile(r'\n ?Получите и Вы свой бесплатный электронный адрес на .+\n'), '\n'),
+]
+
 
 def parse_html(html):
     my_parser = HTML()
     my_parser.feed(html)
     return parse(my_parser.text)
-
 
 # 0 - удалось выделить цитату; 1 - нет
 def parse(text):
@@ -87,15 +102,24 @@ def parse(text):
     for pattern in pre_patterns:
         text = pattern[0].sub(pattern[1], text)
 
-    # выделить цитату в отдельную строку
-    new_text = re.sub(quote_pattern, '\n', text)
-    if len(new_text) == len(text):
-        return text, 1
 
-    text = new_text
+
+    quote = re.search(quote_pattern, text)
+    if quote is not None:
+        quote = quote.group()
+        text = re.sub(quote_pattern, '\n', text)
+    else:
+        return text, '', 1
+
+    for pattern in datetime_patterns:
+
+        datetime = re.search(pattern[0], text)
+        if datetime is not None:
+            quote = datetime.group() + quote
+        text = re.sub(pattern[0], pattern[1], text)
+
     for pattern in post_patterns:
         text = re.sub(pattern[0], pattern[1], text)
 
-    # убрать пустые <blockquote> из разметки
 
-    return text, 0
+    return text, quote, 0
